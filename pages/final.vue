ae4128272c85fa0b2e9223c2d5b5056df038a10e
fantonchini <fanton4ini@gmail.com>
2020-01-29 01:42:24 +0300

    added text on final page

--- pages/final.vue
+++ pages/final.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <textarea name="" id="" cols="30" rows="10">{{text}}</textarea>
+    <textarea name="" id="" cols="50" rows="30">{{text}}</textarea>
   </div>
 </template>
 
@@ -9,6 +9,25 @@ export default {
     data() {
         return {
             room: null
+        }
+    },
+    computed: {
+        text() {
+            const map = this?.room?.maps.find(el => el.banned === false)
+            return 'Team White\n' +
+                this?.room?.captain_one?.name + ' - Капитан команды\n' +
+                this?.room?.teams[this?.room?.captain_one?.name][0] + '\n' +
+                this?.room?.teams[this?.room?.captain_one?.name][1] + '\n' +
+                this?.room?.teams[this?.room?.captain_one?.name][2] + '\n' +
+                this?.room?.teams[this?.room?.captain_one?.name][3] + '\n\n' +
+                'Team Red\n' +
+                this?.room?.captain_two?.name + ' - Капитан команды\n' +
+                this?.room?.teams[this?.room?.captain_two?.name][0] + '\n' +
+                this?.room?.teams[this?.room?.captain_two?.name][1] + '\n' +
+                this?.room?.teams[this?.room?.captain_two?.name][2] + '\n' +
+                this?.room?.teams[this?.room?.captain_two?.name][3] + '\n\n' +
+                'Выбранная карта: ' + map?.name
+
         }
     },
     beforeMount() {
