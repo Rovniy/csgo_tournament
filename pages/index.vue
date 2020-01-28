d3bdb8c4abf972c30db2e900838ebe316d6e0377
fantonchini <fanton4ini@gmail.com>
2020-01-28 22:19:17 +0300

    add homepage + buttons

--- pages/index.vue
+++ pages/index.vue
@@ -1,102 +1,84 @@
 <template>
   <div class="container">
-    <h1>qq</h1>
+    <h1 class="title">xPloit Pracc Helper</h1>
+    <div class="block">
+      <button class="block-button" @click="joinAsAdmin">Я - Админ</button>
+      <button class="block-button" @click="joinAsCaptainFirst">Я - Капитан <span class="block-button_colored">первой</span> команды</button>
+      <button class="block-button" @click="joinAsCaptainSecond">Я - Капитан <span class="block-button_colored">второй</span> команды</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  // data () {
-  //   return {
-  //     task: {
-  //       text: process.env.INTRO_TEXT
-  //     },
-  //     pong: 1
-  //   }
-  // },
-  // computed: {
-  //   isHasText () {
-  //     if (this.task.hasOwnProperty('text') && this.task.text !== null) {
-  //       return this.task.text
-  //     }
-  //     return false
-  //   },
-  //   isHasLink () {
-  //     if (this.task.hasOwnProperty('link') && this.task.link !== null) {
-  //       return (location.protocol + '//' + location.host + '/' + this.task.link)
-  //     }
-  //     return false
-  //   },
-  //   isHasImage () {
-  //     if (this.task.hasOwnProperty('image') && this.task.image !== null) {
-  //       return this.task.image
-  //     }
-  //     return false
-  //   },
-  //   isHasImageClass () {
-  //     if (this.task.hasOwnProperty('imageClass') && this.task.imageClass !== null) {
-  //       return this.task.imageClass
-  //     }
-  //     return false
-  //   },
-  //   isHasTextClass () {
-  //     if (this.task.hasOwnProperty('textClass') && this.task.textClass !== null) {
-  //       return this.task.textClass
-  //     }
-  //     return false
-  //   }
-  // },
-  // beforeMount () {
-  //   this.$socket.on('INCOMING_TASK', (data) => {
-  //     console.log('INCOMING_TASK', data)
-  //     this.task = data
-  //   })
-  //   this.$socket.on('COMPLETED_TASKS', (data) => {
-  //     console.log('COMPLETED_TASKS', data)
-  //     if (data.length === 0) {
-  //       this.task = {
-  //         text: process.env.INTRO_TEXT
-  //       }
-  //     }
-  //   })
-  //   this.$socket.on('GET_LAST_TASK', (data) => {
-  //     console.log('GET_LAST_TASK', data)
-  //     this.task = data
-  //   })
-  // },
-  // mounted () {
-  //   setTimeout(() => {
-  //     this.$socket.emit('task', { type: 'GET_LAST_TASK' })
-  //   }, 1000)
-  // }
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+    joinAsAdmin() {
+      console.log('click-admin')
+    },
+    joinAsCaptainFirst() {
+      console.log('click-cap1')
+    },
+    joinAsCaptainSecond() {
+      console.log('click-cap2')
+    }
+  },
+  beforeMount () {
+    this.$socket.on('INCOMING_TASK', (data) => {
+      console.log('INCOMING_TASK', data)
+      this.task = data
+    })
+    this.$socket.on('COMPLETED_TASKS', (data) => {
+      console.log('COMPLETED_TASKS', data)
+      if (data.length === 0) {
+        this.task = {
+          text: process.env.INTRO_TEXT
+        }
+      }
+    })
+    this.$socket.on('GET_LAST_TASK', (data) => {
+      console.log('GET_LAST_TASK', data)
+      this.task = data
+    })
+  },
+  mounted () {
+    setTimeout(() => {
+      this.$socket.emit('task', { type: 'GET_LAST_TASK' })
+    }, 1000)
+  }
 }
 </script>
 
 <style lang="sass" scoped>
-  .container
-    width: 100vw
-    height: calc(100vh - 60px)
-    display: flex
-    justify-content: center
-    align-items: center
-    flex-direction: column
+.container
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-items: center
+.title
+  font-size: 60px
+  line-height: 80px
+  margin-bottom: 100px
+.block
+  display: flex
+  flex-direction: column
 
-    img
-      max-height: 500px
-
-    h1
-      margin: 20px 0
-      max-width: 70%
-      text-align: center
-
-  .ask
-    height: 14px
-
-  .Base64Spinner
-    @keyframes spinner
-      0%
-        transform: rotateZ(0) rotateY(0) rotateX(0)
-      100%
-        transform: rotateZ(360deg) rotateY(360deg) rotateX(360deg)
-    animation: spinner 2s infinite linear
+  &-button
+    font-size: 30px
+    line-height: 40px
+    margin-bottom: 25px
+    width: 500px
+    border: 2px solid black
+    border-radius: 10px
+    padding: 10px 0
+    background-color: white
+    &:hover
+      background-color: #f7f8fb
 </style>
